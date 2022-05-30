0a67f7e4c31b0875c89e75ed823015239700efc0
Horlakz <horlakz@proton.me>
2022-05-30 18:07:11 +0100

    added logo to page

--- lib/components/baselayout/ActivePageMenu.tsx
+++ lib/components/baselayout/ActivePageMenu.tsx
@@ -1,28 +1,36 @@
+import Image from "next/image";
+
 // component
 import ActivePage from "../index/ActivePage";
 
 // icons
 import { MdOutlineHome } from "react-icons/md";
 
+// images
+import logo from "../../assets/logo.png";
+
 const ActivePageMenu = () => {
   return (
-    <section className="flex-col-center w-[20%] px-8">
-      <ActivePage href="/">
-        <MdOutlineHome size="1.5rem" />
-        <span>Home</span>
-      </ActivePage>
-      <ActivePage href="/music">
-        <MdOutlineHome size="1.5rem" />
-        <span>Home</span>
-      </ActivePage>
-      <ActivePage href="/music">
-        <MdOutlineHome size="1.5rem" />
-        <span>Home</span>
-      </ActivePage>
-      <ActivePage href="/music">
-        <MdOutlineHome size="1.5rem" />
-        <span>Home</span>
-      </ActivePage>
+    <section className="flex-col-center space-y-14 w-[20%] px-8">
+      <div className="w-full">
+        <ActivePage href="/">
+          <MdOutlineHome size="1.5rem" />
+          <span>Home</span>
+        </ActivePage>
+        <ActivePage href="/music">
+          <MdOutlineHome size="1.5rem" />
+          <span>Home</span>
+        </ActivePage>
+        <ActivePage href="/music">
+          <MdOutlineHome size="1.5rem" />
+          <span>Home</span>
+        </ActivePage>
+        <ActivePage href="/music">
+          <MdOutlineHome size="1.5rem" />
+          <span>Home</span>
+        </ActivePage>
+      </div>
+      <Image src={logo} alt="logo" className="mt-10 w-full" />
     </section>
   );
 };
